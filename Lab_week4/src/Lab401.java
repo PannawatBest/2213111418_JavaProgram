2b0e43df6a9ab5792cb7d1214a821f47660037d6
PannawatBest <PannawatBest@gmail.com>
2022-12-15 13:04:19 +0700

    15-Dec-2022

--- Lab_week4/src/Lab401.java
+++ Lab_week4/src/Lab401.java
@@ -2,26 +2,20 @@ import java.util.*;
 
 public class Lab401 {
 	public static void main(String[] args) {
-		
-		Scanner scan = new Scanner(System.in);		
+
+		Scanner scan = new Scanner(System.in);
 		System.out.print("Full Name: ");
 		String fullName = scan.nextLine();
-		
-		if(fullName.contains(" ")) {
+
+		if (fullName.contains(" ")) {
 			int index = fullName.indexOf(" ");
-			String firstName = fullName.substring(0,index).toUpperCase();
+			String firstName = fullName.substring(0, index).toUpperCase();
 			String lastName = fullName.substring(index);
-			System.out.print("First Name: "+firstName
-			        +"\nLast Name: "+lastName);
-			
-		}
-		else {
+			System.out.print("First Name: " + firstName + "\nLast Name: " + lastName);
+
+		} else {
 			System.out.print("Incorrect Name");
 		}
-
-		
-		
-		
 
 	}
 

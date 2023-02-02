c3039c0ce1c6c0f8197001c444919c535aeed5ac
PannawatBest <PannawatBest@gmail.com>
2023-02-02 16:00:16 +0700

    Practice2-02-02-2023

---
+++ OOP_Program/src/Barista.java
@@ -0,0 +1,26 @@
+
+public class Barista {
+	private String name;
+	private char gender;
+	
+	public Barista(String name,char gender) {
+		this.name=name;
+		this.gender=gender;
+	}
+	public Barista() {
+		this.name=" ";
+		this.gender=' ';
+	}
+	public String getName() {
+		return name;
+	}
+	public String getGenderName() {
+		if (gender == 'm' || gender == 'M') {
+			return "Male";
+		} else if (gender == 'f' || gender == 'F') {
+			return "Female";
+		} else {
+			return " ";
+		}
+	}
+}
---
+++ OOP_Program/src/Drink.java
@@ -0,0 +1,55 @@
+
+public class Drink {
+	private int type;
+	private char size;
+	
+	public Drink(int type,char size) {
+		this.type=type;
+		this.size=size;
+	}
+	public Drink() {
+		this.type=0;
+		this.size=' ';
+	}
+	public String getTypeName() {
+		if(type==1) {
+			return "Hot";
+		}else if(type==2) {
+			return "Cold";
+		}else {
+			return "";
+		}
+	}
+	public int getTypePrice() {
+		if(type==1) {
+			return 10;
+		}else if(type==2) {
+			return 20;
+		}else {
+			return 0;
+		}
+	}
+	public String getSizeName() {
+		if(size=='S') {
+			return "Small";
+		}else if(size=='M') {
+			return "Medium";
+		}else if(size=='L'){
+			return "Large";
+		}else {
+			return "";
+		}
+	}
+	public int getSizePrice() {
+		if(size=='S') {
+			return 15;
+		}else if(size=='M') {
+			return 20;
+		}else if(size=='L'){
+			return 2;
+		}else {
+			return 0;
+		}
+	}
+
+}
